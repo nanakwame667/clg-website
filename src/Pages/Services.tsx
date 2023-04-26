ad32ac5d759071a3cd725c52b70c70eb46a2bbef
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2023-04-26 02:36:49 +0000

    finished the services page

--- src/Pages/Services.tsx
+++ src/Pages/Services.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroSection from "../Components/HeroSection";
 import Banner from "../assets/images/services-banner.jpg";
+import { ServicesData } from "../Data/ServicesData";
 
 const Services = () => {
   return (
@@ -11,7 +12,9 @@ const Services = () => {
         </h1>
       </HeroSection>
       <div className="flex flex-col h-96  items-center space-y-12 justify-center w-full text-center">
-        <h1 className="text-secondary-500 font-bold text-[50px]">What we do</h1>
+        <h1 className="text-secondary-500 font-semibold text-[50px]">
+          What we do
+        </h1>
         <p className="text-black text-xl  w-3/4  ">
           At CLG Capital Management, we specialize in providing a comprehensive
           suite of financial services, expertly designed to empower small and
@@ -20,6 +23,31 @@ const Services = () => {
           for success, as we partner with you to achieve your strategic
           objectives.
         </p>
+      </div>
+
+      <div className="flex flex-col items-center mt-6">
+        <h1 className="text-secondary-500 font-semibold text-[36px]">
+          Services
+        </h1>
+        <div className="bg-primary-500 w-[100px] h-[2px] mt-2"></div>
+      </div>
+      <div className="px-24 w-full my-10">
+        {ServicesData.map(({ id, title, text, svg }) => (
+          <div
+            key={id}
+            className="flex flex-row w-full items-center space-x-16 py-20"
+          >
+            <div className="flex items-center justify-center bg-primary-50 px-6 py-6 rounded-full">
+              <img src={svg} alt="" width={150} />
+            </div>
+            <div className="space-y-4 mt-6">
+              <h1 className="text-secondary-500 font-medium text-[28px]">
+                {title}
+              </h1>
+              <p className="text-black text-[20px]">{text}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
