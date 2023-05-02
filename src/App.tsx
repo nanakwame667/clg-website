b903290b0beec59988f709972ca7ab1311c2aef1
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2023-05-02 12:37:19 +0000

    added react helmet to optimize search

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import Header from "./Components/Header";
 import MainHeader from "./Components/MainHeader";
 import Footer from "./Components/Footer";
 import FooterImage from "./Components/FooterImage";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="flex flex-col w-full h-full ">
+      <Helmet>
+        <title>CLG CAPITAL</title>
+        <meta
+          name="description"
+          content="CLG CAPITAL specializes in structured transactions to high growth
+          companies providing transparent capital for the companies we invest
+          in."
+        />
+        <link rel="canonical" href="https://clgcap.com/" />
+      </Helmet>
       <MainHeader />
       <Outlet />
       <FooterImage />
