9dce997523ebf27f6c7f731f35f562b29d47ff67
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2023-05-08 16:09:20 +0000

    updated bio data on teamsdata

--- src/Data/TeamsData.ts
+++ src/Data/TeamsData.ts
@@ -8,13 +8,7 @@ export const TeamsData: TeamsProps[] = [
     title: "Founder & Chief Investment Officer",
     name: "Charles Ofori",
     image: Charles,
-    portfolio: `Previously portfolio manager at Atlas Capital Markets, integral to implementing a strategy which 
-    generated stellar returns in 7 years at Atlas. Prior to Atlas Charles worked for 10 years at Goldman 
-    Sachs London as an Equity Derivatives trader specialising in single stock delta 1. Whilst at GS he 
-    was able to help increase revenue for his trading desk and improved procedures to 
-    exceed management requirements. 
-    Charles is responsible for guiding and executing the firm’s investment strategy and will oversee 
-    research, portfolio construction and investment management.`,
+    portfolio: `Previously a portfolio manager at Atlas Capital Markets LLC. Charles was integral to implementing a strategy which generated stellar returns in 7 years at ACM LLC. Prior to ATLAS, Charles worked for 10 years at Goldman Sachs London as an Equity Derivatives trader specialising in single stock delta 1 trading. Charles is responsible for guiding and executing the firm’s investment strategy and will oversee research, portfolio construction and investment management.`,
   },
   {
     id: 2,
